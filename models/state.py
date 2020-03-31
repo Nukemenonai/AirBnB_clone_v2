2c68ca3b9020dfce30a24978343a5dd9a11b79ed
AdrianCHdz <1214@holbertonschool.com>
2020-03-31 17:21:52 +0000

    [Feature]: Adding salalchemy columns

--- models/state.py
+++ models/state.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+import storage
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade='all, delete')
+
+    @property
+    def cities(self):
+        allcities = storage.all(City)
+        return [n for n in allcities.values() if n.state_id == self.id]
