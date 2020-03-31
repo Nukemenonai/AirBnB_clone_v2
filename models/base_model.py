b1a2a44dfdade820eb7b126e1c583a3f35f02a9e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-03-31 17:19:04 +0000

    [feature] delete method / new sqlalchemy colums

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,21 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+
+    id = Column(String(60), unique=True, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow(),
+                        nullable=False)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow(),
+                        nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -29,7 +38,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,6 +56,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -55,8 +64,16 @@ class BaseModel:
         Return:
             returns a dictionary of all the key values in __dict__
         """
+
         my_dict = dict(self.__dict__)
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict["_sa_instance_state"]
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    del delete(self):
+        """ deletes the current instance from models.storage
+        """
+        models.storage.delete(self)
