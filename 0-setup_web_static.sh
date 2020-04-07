645e6c158b0710d60bfe0abe72bdda71db59fd15
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-07 23:41:00 +0000

    fix soft link and sudo for all commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# this script set up my web server for deplyment
+#this script set up my web server for deplyment
 sudo apt-get update -y
 sudo apt-get install nginx -y
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" >> /data/web_static/releases/test/index.html
-ln -s /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-sed -i "20i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/nginx.conf
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i "20i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/nginx.conf
 sudo service nginx start
