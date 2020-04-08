9fcbb93db59621763188f612f1ca1fbeed996cd6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-08 21:22:21 +0000

    additional configuration for config file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,9 +4,9 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "Holberton School" >> /data/web_static/releases/test/index.html
+sudo echo "Holberton School" >> /data/web_static/releases/test/index.html
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
-sudo sed -i "20i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/nginx.conf
+sudo sed -i "20i\ \tserver_name _;\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/nginx.conf
 sudo service nginx start
