5c5b3fc35f6ae98d26cfcf46331dea39f3ce49cc
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-09 19:58:00 +0000

    fix: smth

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,23 +2,10 @@
 #this script set up my web server for deplyment
 sudo apt-get update -y
 sudo apt-get install nginx -y
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-markup='
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-'
-echo "$markup" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "20i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/nginx.conf
+sed -i "38i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 service nginx start
