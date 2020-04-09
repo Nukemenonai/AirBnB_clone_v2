dd2fee846d6bd508c18e8fc482e29b4bd17866fa
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-09 20:39:37 +0000

    fix smth

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,9 @@ apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
+rm /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "38i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 service nginx start
+exit 0
