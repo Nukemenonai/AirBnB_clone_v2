e770720086ad2abe038b790010b4edec472995ce
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-09 23:02:39 +0000

    fix: deleting symbolic link when its not empty

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #this script set up my web server for deplyment
-sudo apt-get update -y
-sudo apt-get install nginx -y
+apt-get update -y
+apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-rm /data/web_static/current
+rm -rf /data/web_static/current
 ln -s /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "38i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
 service nginx start
+exit 0
