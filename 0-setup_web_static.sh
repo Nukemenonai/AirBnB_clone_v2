08f959a12ad2f144615678007ac3e5a765284b4d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-10 02:40:49 +0000

    fix: applied to sites-enabled

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,8 @@ apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i "38i\ \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
-service nginx start
+sed -i "38i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-enabled/default
+service nginx restart
 exit 0
