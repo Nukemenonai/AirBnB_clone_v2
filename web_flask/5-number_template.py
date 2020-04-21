88fb779e71d5713ed81bce661d17f325c959c06e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-21 22:36:57 +0000

    [task 5] rule for rendering template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""this file stes up a simple flask server """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_route(strict_slashes=False):
+    """ route for default page """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb_route(strict_slashes=False):
+    """ route for /hbnb """
+    return ("HBNB")
+
+
+@app.route('/c/<string:text>')
+def c_is_fun(text, strict_slashes=False):
+    return ("C %s" % text.replace("_", " "))
+
+
+@app.route('/python/<string:text>')
+def python_is_cool(text="is cool", strict_slashes=False):
+    """ variable rules for /python"""
+    return ("Python %s" % text.replace("_", " "))
+
+
+@app.route('/number/<int:number>')
+def is_it_a_number(number, strict_slashes=False):
+    """ rules for number variables routing"""
+    return ("%d is a number" % number)
+
+
+@app.route('/number_template/<int:number>')
+def number_template(number, strict_slashes=False):
+    """ displays an html template on a valid number"""
+    return render_template('templates/5-number.html', number=number)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
