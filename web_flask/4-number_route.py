e23da806062227849929cb95d9774c39bcb3e704
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-21 21:21:05 +0000

    [task 4] handling routing for /number/

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""this file stes up a simple flask server """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_route(strict_slashes=False):
+    """ route for default page """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def hbnb_route(strict_slashes=False):
+    """ route for /hbnb """
+    return ("HBNB")
+
+
+@app.route('/c/<string:text>')
+def c_is_fun(text, strict_slashes=False):
+    return ("C %s" % text.replace("_", " "))
+
+
+@app.route('/python/<string:text>')
+def python_is_cool(text="is cool", strict_slashes=False):
+    """ variable rules for /python"""
+    return ("Python %s" % text.replace("_", " "))
+
+
+@app.route('/number/<int:number>')
+def is_it_a_number(number, strict_slashes=False):
+    """ rules for number variables routing"""
+    return ("%d is a number" % number)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
