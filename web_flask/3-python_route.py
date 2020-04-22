13c3682fa552b7fab316762ca4828c39d1448808
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-22 02:13:42 +0000

    fix: tried another method for defaults

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,8 +25,8 @@ def c_is_fun(text, strict_slashes=False):
     return ("C %s" % escape(text.replace("_", " ")))
 
 
-@app.route('/python/<string:text>')
-def python_is_cool(text="is cool", strict_slashes=False):
+@app.route('/python/<string:text>', defaults={'text':'is cool'})
+def python_is_cool(text, strict_slashes=False):
     """ variable rules for /python"""
     return ("Python %s" % escape(text.replace("_", " ")))
 
